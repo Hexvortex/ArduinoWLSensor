1954b237847853352c9957f14f766e4986aa77b2
Hexvortex <66158651+Hexvortex@users.noreply.github.com>
2021-03-22 01:29:29 +0530

    First commit

--- ldr_sensor.ino
+++ ldr_sensor.ino
@@ -1,14 +1,25 @@
-int light;
-
+int ldr;
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(light);
-  delay(100);
-
+  ldr = analogRead(A0);
+  
+  if (ldr < 50) {
+      digitalWrite(13, HIGH);
+      delay(5000);
+      digitalWrite(13, LOW);
+      delay(10000);
+    
+  } else {
+    digitalWrite(13, LOW);
   }
+  Serial.println(motor);
+  delay(60);
+
+}
